9dc8471256915e5cbeee3ea1c6840d654cb25047
tyodev <deve@tyo.com.au>
2017-03-07 12:05:27 +1000

    Make it a collection submodules / libraries

--- CommonAndroid/src/au/com/tyo/android/gesture/PinchListener.java
+++
@@ -1,7 +0,0 @@
-package au.com.tyo.android.gesture;
-
-public interface PinchListener {
-	
-	void onScaleChanged(int mode, float mScale);
-
-}
--- CommonAndroid/src/au/com/tyo/android/services/DownloaderInterface.java
+++
@@ -1,5 +0,0 @@
-package au.com.tyo.android.services;
-
-public interface DownloaderInterface<FileType, ContainerType> {
-	void handleResult(ContainerType container, FileType file);
-}
